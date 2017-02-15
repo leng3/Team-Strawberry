11990a4b197dd1ff159aeff0b09bfe6f3ff5454d
leng3 <leng3@stuy.edu>
2017-02-14 21:44:54 -0500

    Add files via upload

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -33,20 +33,37 @@ public class MergeSortTester
 	int[] one = {1};
 	int[] ten = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int[] hundred = new int[100];
-	int[] huge = new int[100000]
-;
+	int[] T = new int[1000];
+	int[] TT = new int[10000];
+	int[] HT = new int[100000];
+	int[] M = new int[1000000];
+	
 	for (int x = 0; x < hundred.length; x++){
 	    hundred[x] = (int)(Math.random() * 100);
 	}
 
-	for (int x = 0; x < huge.length; x++){
-	    huge[x] = (int)(Math.random() * 100000);
+	for (int x = 0; x < T.length; x++){
+	    T[x] = (int)(Math.random() * 1000);
+	}
+	
+	for (int x = 0; x < TT.length; x++){
+	    TT[x] = (int)(Math.random() * 10000);
+	}
+	
+	for (int x = 0; x < HT.length; x++){
+	    HT[x] = (int)(Math.random() * 100000);
+	}
+
+	for (int x = 0; x < M.length; x++){
+	    M[x] = (int)(Math.random() * 1000000);
 	}
 	
 	long startTime = System.nanoTime();
+
 	for (int x = 0; x < 10; x+=1){
 	    MergeSort.sort(huge);
 	}
+	
 	long endTime = System.nanoTime();
 	System.out.println("Time in seconds: " + (((endTime - startTime)/1000000000.0))/10);
     }//end main
